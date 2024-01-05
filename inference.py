41bbfb526e09175bf420f2ead9e85c06d4bb9edc
maksmikhilov <maksmikhilov01@gmail.com>
2024-01-05 22:01:41 +0300

    fix

--- inference.py
+++ inference.py
@@ -11,7 +11,7 @@ pipeline = transformers.pipeline(
     model_kwargs={"torch_dtype": torch.float16, "load_in_4bit": True},
 )
 
-messages = [{"role": "user", "content": "Explain what a Mixture of Experts is in less than 100 words."}]
+messages = [{"role": "user", "content": "Сколько дней в году?"}]
 prompt = pipeline.tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
 outputs = pipeline(prompt, max_new_tokens=256, do_sample=True, temperature=0.7, top_k=50, top_p=0.95)
 print(outputs[0]["generated_text"])
