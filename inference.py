71442311102df15990320c04740dd7a5463ba1d7
maksmikhilov <maksmikhilov01@gmail.com>
2024-01-05 21:23:53 +0300

    fix

--- inference.py
+++ inference.py
@@ -2,25 +2,16 @@ from transformers import AutoTokenizer
 import transformers
 import torch
 
+model = "mistralai/Mixtral-8x7B-Instruct-v0.1"
 
-model_id = "./mixtral-8x7b-32kseqlen"
-
-model = transformers.AutoModelForCausalLM.from_pretrained(
-    f'{model_id}/config.json',
+tokenizer = AutoTokenizer.from_pretrained(model)
+pipeline = transformers.pipeline(
+    "text-generation",
+    model=model,
+    model_kwargs={"torch_dtype": torch.float16, "load_in_4bit": True},
 )
-model.load_state_dict(torch.load(f'{model_id}/'))
 
-tokenizer = AutoTokenizer.from_pretrained(model_id)
-generate_text = transformers.pipeline(
-    model=model, tokenizer=tokenizer,
-    return_full_text=False,  # if using langchain set True
-    task="text-generation",
-    # we pass model parameters here too
-    temperature=0.1,  # 'randomness' of outputs, 0.0 is the min and 1.0 the max
-    top_p=0.15,  # select from top tokens whose probability add up to 15%
-    top_k=0,  # select from top 0 tokens (because zero, relies on top_p)
-    max_new_tokens=512,  # max number of tokens to generate in the output
-    repetition_penalty=1.1  # if output begins repeating increase
-)
-outputs = generate_text('hi, who you?')
+messages = [{"role": "user", "content": "Explain what a Mixture of Experts is in less than 100 words."}]
+prompt = pipeline.tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
+outputs = pipeline(prompt, max_new_tokens=256, do_sample=True, temperature=0.7, top_k=50, top_p=0.95)
 print(outputs[0]["generated_text"])
