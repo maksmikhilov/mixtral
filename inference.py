d7c1b6e42f3dc254d4d871b0467b59b4e56dc097
maksmikhilov <maksmikhilov01@gmail.com>
2024-01-06 10:45:42 +0300

    fix

--- inference.py
+++ inference.py
@@ -91,7 +91,6 @@ inputs = tokenizer(prompt, return_tensors="pt").to(device)
 output = model.generate(
     **inputs,
     max_new_tokens=2048,
-    temperature=0.0,
-    do_sample=True
+    temperature=0.1
     )
 print(tokenizer.decode(output[0], skip_special_tokens=True))
