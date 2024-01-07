08c3899452205ead6cfaa9ea60770f084b8a1ee2
maksmikhilov <maksmikhilov01@gmail.com>
2024-01-07 06:29:19 +0300

    fix

--- inference.py
+++ inference.py
@@ -11,14 +11,15 @@ quantization_config = BitsAndBytesConfig(
 
 tokenizer = AutoTokenizer.from_pretrained(model_id)
 model = AutoModelForCausalLM.from_pretrained(model_id, quantization_config=quantization_config)
-prompt1 = get_prompt('Вычисли GBOU для Урала за каждый месяц 2020 года и выведи значения в виде таблицы.')
-prompt2 = get_prompt('Круговая диаграмма подключений по каждому имени сервиса за 2020 год')
+prompt = get_prompt('Вычисли GBOU для Урала за каждый месяц 2020 года и выведи значения в виде таблицы.')
 
-inputs = tokenizer([prompt1, prompt2], return_tensors="pt").to(device)
+inputs = tokenizer(prompt, return_tensors="pt").to(device)
 output = model.generate(
     **inputs,
     max_new_tokens=2048,
-    temperature=0.1
+    temperature=0.1,
+    top_p=0.95,
+    top_k=40,
+    repetition_penalty=1.1
     )
 print(tokenizer.decode(output[0], skip_special_tokens=True))
-print(tokenizer.decode(output[1], skip_special_tokens=True))
