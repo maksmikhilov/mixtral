b5067f009090d3af5d653d0bfcad4a5480308f71
maksmikhilov <maksmikhilov01@gmail.com>
2024-02-06 19:38:47 +0300

    fix

--- vLLM.py
+++ vLLM.py
@@ -7,7 +7,7 @@ prompts = [
 ]
 sampling_params = SamplingParams(temperature=0.8, top_p=0.95)
 
-llm = LLM(model="mistralai/Mixtral-8x7B-v0.1", tensor_parallel_size=8, download_dir='/home/ubuntu/models_weight')
+llm = LLM(model="mistralai/Mixtral-8x7B-v0.1", tensor_parallel_size=8, download_dir='/home/ubuntu/models_weight', gpu_memory_utilization=0.5)
 
 outputs = llm.generate(prompts, sampling_params)
 
