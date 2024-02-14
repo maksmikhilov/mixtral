316e18738b75e8d150284e9321ff2a17e8b7c283
maksmikhilov <maksmikhilov01@gmail.com>
2024-02-15 00:08:03 +0300

    fix

--- deepspeed.py
+++ deepspeed.py
@@ -1,7 +1,12 @@
 import mii
+"""
 client = mii.server(
     "TheBloke/Mixtral-8x7B-Instruct-v0.1-GPTQ",
     deployment_name="generate",
     enable_restful_api=True,
     restful_api_port=8000,
 )
+"""
+pipe = mii.pipeline("TheBloke/Mixtral-8x7B-Instruct-v0.1-GPTQ")
+response = pipe(["[INST] DeepSpeed is? [/INST]", "DeepSpeed is?"], max_new_tokens=128)
+print(response)
