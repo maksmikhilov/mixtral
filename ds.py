4ee8cc47eae1b28630e9a2d4f06a0adddcedc9ba
maksmikhilov <maksmikhilov01@gmail.com>
2024-02-15 05:19:22 +0300

    fix

--- ds.py
+++ ds.py
@@ -2,6 +2,5 @@ import mii
 client = mii.serve(
     "TheBloke/Mixtral-8x7B-Instruct-v0.1-GPTQ",
     deployment_name="generate",
-    enable_restful_api=True,
-    restful_api_port=8000,
+    enable_restful_api=True
 )
