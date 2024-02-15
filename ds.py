f66a6b3460f332a31cd80eacdc2d59349d1f386a
maksmikhilov <maksmikhilov01@gmail.com>
2024-02-15 08:48:07 +0300

    fix

--- ds.py
+++ ds.py
@@ -1,14 +1,7 @@
 import mii
-"""
 client = mii.serve(
-    "TheBloke/Mixtral-8x7B-Instruct-v0.1-GPTQ",
+    "mistralai/Mistral-7B-Instruct-v0.2",
     deployment_name="generate",
-    enable_restful_api=True
+    enable_restful_api=True,
+    tensor_parallel=2,
 )
-"""
-pipe = mii.pipeline("TheBloke/Mixtral-8x7B-Instruct-v0.1-GPTQ")
-response = pipe([
-    "[INST] Как получать в логах только нужные поля при использовании обработчика loguru с параметром serialize=True [/INST]",
-    "Как получать в логах только нужные поля при использовании обработчика loguru с параметром serialize=True"
-    ], max_new_tokens=1500, tensor_parallel=2)
-print(response)
