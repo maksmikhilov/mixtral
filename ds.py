19f886e098d4a0e5596ef6bb09dc73ef6cd9b734
maksmikhilov <maksmikhilov01@gmail.com>
2024-02-15 05:37:36 +0300

    fix

--- ds.py
+++ ds.py
@@ -10,5 +10,5 @@ pipe = mii.pipeline("TheBloke/Mixtral-8x7B-Instruct-v0.1-GPTQ")
 response = pipe([
     "[INST] Как получать в логах только нужные поля при использовании обработчика loguru с параметром serialize=True [/INST]",
     "Как получать в логах только нужные поля при использовании обработчика loguru с параметром serialize=True"
-    ], max_new_tokens=1500)
+    ], max_new_tokens=1500, tensor_parallel=2)
 print(response)
