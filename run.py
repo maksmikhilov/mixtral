b5d8727880d30695f7dd1d806b20bea38baae9e3
maksmikhilov <maksmikhilov01@gmail.com>
2024-02-15 15:02:10 +0300

    fix

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
 import mii
-pipe = mii.pipeline("mistralai/Mistral-7B-Instruct-v0.2", tensor_parallel=4)
+pipe = mii.pipeline("mistralai/Mistral-7B-Instruct-v0.2", tensor_parallel=2)
 response = pipe(["[INST] Как получать в логах только нужные поля при использовании обработчика loguru с параметром serialize=True [/INST]", "Как получать в логах только нужные поля при использовании обработчика loguru с параметром serialize=True"], max_new_tokens=1000)
 print(response)
